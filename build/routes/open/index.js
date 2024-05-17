4212ed9e4cbd580207d65825d674c7919233e13a
BRODI MATHERLY <69179454+amatherly@users.noreply.github.com>
2024-05-17 13:50:18 -0700

    Build and docs
    Build and docs

--- build/routes/open/index.js
+++ build/routes/open/index.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.openRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const message_1 = require("./message");
+const books_1 = require("./books");
 const openRoutes = express_1.default.Router();
 exports.openRoutes = openRoutes;
 openRoutes.use('/message', message_1.messageRouter);
+openRoutes.use('/books', books_1.booksRouter);
 //# sourceMappingURL=index.js.map

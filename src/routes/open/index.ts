ad1db6793fa773788cdb71780ab53a6d28880a81
Calvin Beardemphl <your.gr8.pal.cal@gmail.com>
2024-05-17 18:03:14 -0700

    Revert "Merge pull request #15 from Listoniann/auth"
    This reverts commit f5ee83bf8e5f41f46e3b0b630953b26492634cf0, reversing
    changes made to 43c78a489953b90b53db142b88eca3cc12d1d1fa.

--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -1,9 +1,11 @@
 import express, { Router } from 'express';
 
 import { messageRouter } from './message';
-// import { booksRouter } from '../closed/books';
+import { booksRouter } from './books';
+
 const openRoutes: Router = express.Router();
 
 openRoutes.use('/message', messageRouter);
-// openRoutes.use('/books', booksRouter);
+openRoutes.use('/books', booksRouter);
+
 export { openRoutes };

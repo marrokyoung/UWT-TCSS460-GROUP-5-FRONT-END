70b6724f2c7f572ccadb3b0579fb21f21011185e
calvinpalvin <77321790+calvinpalvin@users.noreply.github.com>
2024-05-17 19:20:21 -0700

    Update index.ts

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -2,10 +2,14 @@ import express, { Router } from 'express';
 
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
+import { booksRouter } from './books';
 import {pool} from "../../core/utilities";
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
+closedRoutes.use('/books', checkToken, booksRouter);
+
+const openRoutes: Router = express.Router();
 
 export { closedRoutes };

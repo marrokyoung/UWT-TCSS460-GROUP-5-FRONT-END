d6b4572e5b8277fdc80ac3dcb7396963bf22a74a
Slavik <rutkovsky2003@gmail.com>
2024-05-30 14:01:20 -0700

    changed localhost to 4000

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:4001/auth/login', {
+    const response = await fetch('http://localhost:4000/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
